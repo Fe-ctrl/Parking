500635561d6b6cc5bc7636b53aeded25d580bf1d
Felipe Dagnoni <felipedagnoni@hotmail.com>
2026-03-10 18:42:05 -0300

    improving the global exception error

--- src/main/java/parkingproject/com/Parking/exception/GlobalExceptionError.java
+++ src/main/java/parkingproject/com/Parking/exception/GlobalExceptionError.java
@@ -2,14 +2,36 @@ package parkingproject.com.Parking.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionError {
 
+    // Missing or wrong query params (e.g. forgot to send ?plate=...)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, Object>> handleMissingParam(MissingServletRequestParameterException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Missing Parameter");
+        response.put("details", "Required parameter '" + ex.getParameterName() + "' is missing");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // Wrong type for a param (e.g. type=abc instead of type=1)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, Object>> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Invalid Parameter");
+        response.put("details", "Parameter '" + ex.getName() + "' has an invalid value: " + ex.getValue());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // All other exceptions (DB errors, etc.)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
         Map<String, Object> response = new HashMap<>();
